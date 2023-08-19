bec2e0d422e36020ec436fbba7d174a1b6871487
Jan M <129630363+potatoesAndMolasses@users.noreply.github.com>
2023-08-19 20:39:49 -0300

    Add files via upload
    O programa pergunta 10 números e ao final apresenta a soma deles.
    Eu montei o código com duas estruturas diferentes (while e for), os dois códigos tem a mesma finalidade.

---
+++ Aprendendo C/soma_dez_valores.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Faca um programa que peca ao usuario para digitar 10 valores e some-os
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    
+    /*
+    int valores = 1;
+    int soma, numeros_digitados = 0;
+    
+    while (valores <= 10) {
+        
+        printf("Digite o %d° valor a ser somado:\n", valores);
+        scanf("%d", &numeros_digitados);
+        soma += numeros_digitados;
+        valores++;
+    }
+    printf("A soma de todos os valores digitados é: %d", soma);
+    */
+   
+   /* 
+    int quantidade_numeros = 10;
+    int soma, valor = 0;
+    
+    printf("Vamos somar %d, valores.\n", quantidade_numeros);
+    
+    for(int i = 0; i < quantidade_numeros; i++) {
+        printf("Digite o %d valor:\n", i + 1);
+        scanf("%d", &valor);
+        soma += valor;
+    }
+    printf("A soma dos valores é: %d", soma);
+    */
+    
+    return 0;
+}
+
